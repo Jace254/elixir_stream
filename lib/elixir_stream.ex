e58275b27fab219785c6161fe1e0f4e7ca839ef3
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-06-12 21:50:30 +0300

    chore: init

---
+++ lib/elixir_stream.ex
@@ -0,0 +1,18 @@
+defmodule ElixirStream do
+  @moduledoc """
+  Documentation for `ElixirStream`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirStream.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
